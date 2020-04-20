6fbf4bf2c70093566eb4c88ff59a4f105aabd5d4
Saeed Siddik <saeed.siddik@iit.du.ac.bd>
2020-04-20 18:30:40 +0600

    Update index.php including data table show
    - Add drop down option to select which newspaper in selected to view the article
    - Refactor some php code

--- index.php
+++ index.php
@@ -13,9 +13,11 @@ if ($conn->connect_error) {
 ?>
 <!doctype html>
 <html lang="en">
+
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
@@ -26,7 +28,7 @@ if ($conn->connect_error) {
   <body>
     <div class="container">
 		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-			<a class="navbar-brand" href="#"> News Crawler of Asgaard Lab</a>
+			<a style="text-align:center" class="navbar-brand" href="#"> News Crawler of Asgaard Lab:   Topic: Covid-19</a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
@@ -43,58 +45,86 @@ if ($conn->connect_error) {
 		
 		<div class="row">
 			<div class="col">
+			
 				<div class="card">
-					<div class="card-header">
-						Top News
-					</div>
-					
 					<div class="card-body">
-						<div class="table-responsive">
-							<table class="table table-bordered table-hover">
-								<thead class="thead-dark">
-									<tr>
-										<th scope="col">Sl. </th>
-										<th scope="col">Title</th>
-									</tr>
-								</thead>
-								<tbody>
-								<?php
-								$sql = "SELECT * FROM cnn_news";
-
-								$result = $conn->query($sql);
-
-								if ($result->num_rows > 0) { ?>
-
-								<?php
-									// output data of each row
-									while($row = $result->fetch_assoc()) { ?>
-										<tr>
-											<th scope="row"><?php echo $row['id']; ?> </th>
-											<td>
-												<p>
-													<button class="btn" type="button" data-toggle="collapse" data-target="#new_description<?php echo $row['id']; ?>" aria-expanded="false" aria-controls="new_description">
-														<?php echo $row['title']; ?> 
-													</button>
-													<?php	 echo (is_null($row['authors'])) ? "" : $row['authors']; ?>
-												</p>
-												<div class="collapse" id="new_description<?php echo $row['id']; ?>">
-													<div class="card card-body">
-														<?php echo $row['description']; ?>
-													</div>
-												</div>
-											</td>
-										</tr>								
-									<?php }
-								} else {
-									echo "Found 0 results.";
-								}
-								?>
-
-								</tbody>
-							</table>
-						</div><!-- /table-responsive -->
+						<form action="index.php" method="post" class="form-horizontal">
+							<div class="form-row align-items-center">
+								<div class="col-auto">
+									<label class="sr-only" for="type">Name</label>
+									<select name="type" class="form-control mb-2">
+										<option value="">Select Type</option>
+										<option value="cnn">CNN</option>
+										<option value="dailystar">Daily Star</option>
+									</select>
+								</div>
+								
+								<div class="col-auto">
+									<button type="submit" name="btn_news" class="btn btn-primary mb-2">Submit</button>
+								</div>
+							</div>
+						</form>
 					</div>
-				</div><!-- /.card -->
+				</div>
+				<?php if( isset( $_POST['btn_news'] ) ) { ?>
+					<div class="card">
+						<div class="card-header">
+							<?php echo ucfirst( $_POST['type'] ); ?> News
+						</div>
+						
+						<div class="card-body">
+							<div class="table-responsive">
+								<table class="table table-bordered table-hover">
+									<thead class="thead-dark">
+										<tr>
+											<th scope="col">Sl. </th>
+											<th scope="col">Title</th>
+										</tr>
+									</thead>
+									<tbody>
+									<?php
+									$type = isset( $_POST['type'] ) ? $_POST['type'] : '';
+									
+									$sql = "SELECT * FROM news WHERE type= '". $type ."' ";
+
+									$result = $conn->query($sql);
+
+									if ($result->num_rows > 0) { ?>
+
+									<?php
+										$count = 1;
+										// output data of each row
+										while($row = $result->fetch_assoc()) { ?>
+											<tr>
+												<th scope="row"><?php echo $count; ?></th>
+												<td>
+													<p>
+														<button class="btn" type="button" data-toggle="collapse" data-target="#new_description<?php echo $row['id']; ?>" aria-expanded="false" aria-controls="new_description">
+															<?php echo $row['title']; ?>
+														</button>
+													<?php	header('Content-Type: text/html; charset=utf-8');
+ echo (is_null($row['authors'])) ? "" : $row['authors']; ?>
+													</p>
+													<div class="collapse" id="new_description<?php echo $row['id']; ?>">
+														<div class="card card-body">
+															<?php header('Content-Type: text/html; charset=utf-8');
+echo $row['description']; ?>
+														</div>
+													</div>
+												</td>
+											</tr>								
+										<?php $count++; }
+									} else {
+										echo "Found 0 results.";
+									}
+									?>
+
+									</tbody>
+								</table>
+							</div><!-- /table-responsive -->
+						</div>
+					</div><!-- /.card -->
+				<?php } ?>
 			</div><!-- /.col -->
 		</div><!-- /.row -->
 	</div><!-- /.container -->
