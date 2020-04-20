a3a2a30441dc5b6e03dbb2d1074da7e6d1ff3aad
Saeed Siddik <saeed.siddik@iit.du.ac.bd>
2020-04-20 14:34:51 +0600

    Update crawler assignment for asgaard
    Add appropriate comments for code segment and refactor some lines

--- asgaard_crawler.py
+++ asgaard_crawler.py
@@ -12,7 +12,7 @@ def database_connection_start(host, user, password, database_name):
 	
 	return db
 
-
+#data preprocessing to store in database
 def article_preprocessing_for_database(text_data):
 	processed_data = pymysql.escape_string(text_data)
 	processed_data = processed_data.replace("\\n" , "<br />")
@@ -21,41 +21,38 @@ def article_preprocessing_for_database(text_data):
 	processed_data = processed_data.replace("%" , "\%")
 	return processed_data
 
-
+#newspaper content loading based on the url
 def newspaper_article_build(newspaper_url):
-	#newspaper content loading
 	newspaper_content_build = newspaper.build(newspaper_url, memoize_articles=False)
 	return newspaper_content_build
 
 def individual_article_process_for_db_store(db, table_name, newspaper_content_build, article_topic_to_show, number_of_article_to_show):
-	news_counter = 0
-
-	database_clear(db, table_name)
-
+	news_counter = 0	# to check the maximum number of article in show	
+	database_clear(db, table_name)	# clear the database table content for storing fresh article 
 	cursor = db.cursor()
-	for article in newspaper_content_build.articles[0:10]:
-		if (news_counter >=number_of_article_to_show):
+	
+	for article in newspaper_content_build.articles:		# loop for investigating entire articles
+		if (news_counter >=number_of_article_to_show):		# check the maximum data 
 		    break
-		try:
-		    article.download()
-		    article.parse()
-		    if article_topic_to_show in article.text:
+		try:							# handla exception 
+		    article.download()					# download the article details
+		    article.parse()					# parse the article 
+		    if article_topic_to_show in article.text:		# check the targeted topic in article
 		        news_counter += 1
-		        id = news_counter
+		        id = news_counter				# news ID 
 		        title = article_preprocessing_for_database(article.title)
 		        description = article_preprocessing_for_database(article.text)
 		        url = article.url
-		        authors = article_preprocessing_for_database(', '.join([str(elem) for elem in article.authors]))
-		        article_insert_in_database(db, table_name, id,title,description, url, authors)
+		        authors = article_preprocessing_for_database(', '.join([str(elem) for elem in article.authors]))	#hence, authors is al ist, it needed to ve converted to String
+		        article_insert_in_database(db, table_name, id,title,description, url, authors)		# call database insertion query
 		except Exception as e:
 		    print (e)
 		    continue
 	print ("END")
 
-
+#insert newspaper article to database table
 def article_insert_in_database(db, table_name, id,title,description, url, authors):
-#insert cnn article to database table
-       
+      
 	cursor = db.cursor()
 	sql = "INSERT INTO %s(id, title, description, authors) VALUES ('%s','%s','%s', '%s')"%(table_name,id, title,description, authors)
 	try:
@@ -67,7 +64,7 @@ def article_insert_in_database(db, table_name, id,title,description, url, author
 # 		print (description)
 		db.rollback()
 		
-
+# database content clear for fresh data
 def database_clear(db, table_name):
     cursor = db.cursor()
     sql1 = "TRUNCATE TABLE %s"%table_name 
@@ -93,22 +90,25 @@ def article_show_in_console(db, table):
 	except:
 		print ("error in data show")
 
-
+#database connection close
 def database_connection_close(db):
-	#database connection close
 	db.close()
 
 def main():
 	print ("program start for article crawler in asgaard lab assignment")
-	newspaper_url = "https://edition.cnn.com"
-	newspaper_content_build = newspaper_article_build(newspaper_url)
+	newspaper_url = "https://edition.cnn.com"				# targeted newspaper URL for articel collection
+	newspaper_content_build = newspaper_article_build(newspaper_url)	# build the newspaper URL using python-newspaper3k library
 
-	db = database_connection_start("localhost","root","1987","crawler")
-	individual_article_process_for_db_store(db, "cnn_news", newspaper_content_build, "Covid-19", 25)
+	db = database_connection_start("localhost","root","1987","crawler")	# connect MySQL database with credintial 
+	
+	topic_for_search = "Covid-19"
+	number_of_article_to_show = 25
+	individual_article_process_for_db_store(db, "cnn_news", newspaper_content_build, topic_for_search, number_of_article_to_show)	
 	
 	database_connection_close(db)
 	print("Program end")
 
+# Python main function to start the code
 if __name__ == '__main__':
     main()
 
